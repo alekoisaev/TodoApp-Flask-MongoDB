bfe271de8351e0719d98bbb31279232d40702b41
ALeko <42478187+alekoisaev@users.noreply.github.com>
2021-03-17 01:30:49 +0400

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, url_for, redirect
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+app.config['MONGO_URI'] = 'mongodb+srv://aleko:123421@cluster0.qjwu5.mongodb.net/mydb?retryWrites=true&w=majority'
+mongo = PyMongo(app)
+
+todos = mongo.db.todos
+
+
+@app.route('/')
+def index():
+    db_todo = todos.find()
+    return render_template('index.html', todos=db_todo)
+
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    new_todo = request.form.get('new-todo')
+    todos.insert_one({'text': new_todo, 'complete': 'No'})
+    return redirect(url_for('index'))
+
+
+@app.route('/complete/<oid>')
+def complete_todo(oid):
+    td = todos.find_one({'_id': ObjectId(oid)})
+    td['complete'] = 'Completed'
+    todos.save(td)
+    return redirect(url_for('index'))
+
+
+@app.route('/delete/<oid>')
+def delete_todo(oid):
+    td = todos.find_one({'_id': ObjectId(oid)})
+    todos.remove(td)
+    return redirect(url_for('index'))
